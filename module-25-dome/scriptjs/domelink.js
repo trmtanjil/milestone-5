add12f354709fec539da48e5ceafe8744656ea6b
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-02-22 09:07:03 +0600

    id and class commit

--- module-25-dome/scriptjs/domelink.js
+++ module-25-dome/scriptjs/domelink.js
@@ -1,4 +1,3 @@
- 
 
 let informations = document.getElementsByTagName('li');
 for(let information of informations){
@@ -9,5 +8,21 @@ for(let information of informations){
 
 let headdings = document.getElementsByTagName('h1');
 for(let headding of headdings ){
-    console.log(headding.innerHTML);
+    // console.log(headding.innerHTML);
+}
+//!       getElementByid
+let changeHeading = document.getElementById('dombabaa').innerHTML = 'give me aro dom baba dom';
+console.log(changeHeading);
+
+// console.log(document.getElementById('fevorite-place'));
+// console.log(document.getElementById('fevorite-place').innerText ='you my enjoble person');
+
+
+//!       getElementByClass
+
+let fruits = document.getElementsByClassName('fevoritefrt');
+// console.log(fruits);
+let fruitss = document.getElementsByClassName('fevoritefrt');           //!We can also get inner text.
+for(let fruit of fruitss){
+    console.log(fruit.innerHTML);
 }
