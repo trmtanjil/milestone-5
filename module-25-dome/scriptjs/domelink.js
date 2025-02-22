a825ff83427f15013c591396405e98c146f759f1
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-02-22 09:46:23 +0600

    queryselector commit

--- module-25-dome/scriptjs/domelink.js
+++ module-25-dome/scriptjs/domelink.js
@@ -12,7 +12,7 @@ for(let headding of headdings ){
 }
 //!       getElementByid
 let changeHeading = document.getElementById('dombabaa').innerHTML = 'give me aro dom baba dom';
-console.log(changeHeading);
+// console.log(changeHeading);
 
 // console.log(document.getElementById('fevorite-place'));
 // console.log(document.getElementById('fevorite-place').innerText ='you my enjoble person');
@@ -24,5 +24,31 @@ let fruits = document.getElementsByClassName('fevoritefrt');
 // console.log(fruits);
 let fruitss = document.getElementsByClassName('fevoritefrt');           //!We can also get inner text.
 for(let fruit of fruitss){
-    console.log(fruit.innerHTML);
+    // console.log(fruit.innerHTML);
 }
+
+//! queryselectorAll & queryselector  class
+
+let selectAll =document.querySelectorAll('.domIsdom li');
+// console.log(selectAll);
+// console.log(selectAll.innerHTML);
+
+let selecsingle =document.querySelector('.domIsdom li');
+// console.log(selecsingle.innerHTML);
+
+for(let classs of selectAll){
+    // console.log(classs);
+    // console.log(classs.innerHTML);
+}
+//! queryselectorAll & queryselector  id
+let idselect = document.querySelectorAll('#selectId li');
+// console.log(idselect);
+
+let idselects = document.querySelector('#selectId li');
+// console.log(idselects);
+
+for(let ids of idselect){
+    // console.log(ids.innerHTML);
+    // console.log(ids);
+}
+
