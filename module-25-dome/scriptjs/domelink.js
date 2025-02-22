02250a5b3e1436bec95b8408182c5eb08b948c9d
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-02-22 07:56:29 +0600

    dom getElementByTagName commit

--- module-25-dome/scriptjs/domelink.js
+++ module-25-dome/scriptjs/domelink.js
@@ -1,2 +1,13 @@
-console.log(document);
-console.log(document.getElementsByTagName('li'));
+ 
+
+let informations = document.getElementsByTagName('li');
+for(let information of informations){
+    // console.log(information);    
+    // console.log(information.innerText);
+}
+// console.log(informations);
+
+let headdings = document.getElementsByTagName('h1');
+for(let headding of headdings ){
+    console.log(headding.innerHTML);
+}
